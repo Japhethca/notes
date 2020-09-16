4e5251e4e578edcf8a1d5ec500df900a90b00297
japhethca <chidiebere.anyigor@gmail.com>
2020-09-16 18:53:18 +0100

    formated file

--- src/store/Provider.tsx
+++ src/store/Provider.tsx
@@ -3,42 +3,42 @@ import { useStoreReducer, setActiveUser, logout } from "./reducer";
 import { getUserfromCookie, removeAuthToken } from "../libs/cookie";
 
 interface IStoreContext {
-	state?: any;
-	dispatch?: any;
-	handleLogout?: any;
-	handleLogin?: any;
+  state?: any;
+  dispatch?: any;
+  handleLogout?: any;
+  handleLogin?: any;
 }
 
 const defaultValues: IStoreContext = {};
 export const storeContext = createContext(defaultValues);
 
 const Provider = ({ children }) => {
-	const [state, dispatch] = useStoreReducer();
-	useEffect(() => {
-		const user = getUserfromCookie();
-		if (!user) {
-			dispatch(logout(null));
-			return () => {};
-		}
-		dispatch(setActiveUser(user));
-	}, [dispatch]);
+  const [state, dispatch] = useStoreReducer();
+  useEffect(() => {
+    const user = getUserfromCookie();
+    if (!user) {
+      dispatch(logout(null));
+      return () => {};
+    }
+    dispatch(setActiveUser(user));
+  }, [dispatch]);
 
-	const handleLogout = () => {
-		removeAuthToken();
-		dispatch(logout(null));
-	};
+  const handleLogout = () => {
+    removeAuthToken();
+    dispatch(logout(null));
+  };
 
-	const handleLogin = (user: NoteUser) => {
-		dispatch(setActiveUser(user));
-	};
+  const handleLogin = (user: NoteUser) => {
+    dispatch(setActiveUser(user));
+  };
 
-	return (
-		<storeContext.Provider
-			value={{ state, dispatch, handleLogout, handleLogin }}
-		>
-			{children}
-		</storeContext.Provider>
-	);
+  return (
+    <storeContext.Provider
+      value={{ state, dispatch, handleLogout, handleLogin }}
+    >
+      {children}
+    </storeContext.Provider>
+  );
 };
 
 export default Provider;
