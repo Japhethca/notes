d2978e37a157b78c39556e8a7d19d12c19b1fdb9
japhethca <chidiebere.anyigor@gmail.com>
2020-09-16 18:48:45 +0100

    change color name

--- src/components/NoteView.tsx
+++ src/components/NoteView.tsx
@@ -18,9 +18,10 @@ const NoteForm: FunctionComponent<NoteFormProps> = ({ note }) => {
         fontSize={["2xl", "2xl", "3xl"]}
         p={[2, 4, 6]}
         placeholder="Note Title"
-        bg="#fff"
+        bg="white"
         mt="0"
         mb="2"
+        color="gray.500"
         borderRadius="5px"
       >
         {note.title}
@@ -29,23 +30,24 @@ const NoteForm: FunctionComponent<NoteFormProps> = ({ note }) => {
         fontSize={["md", "xl", "xl"]}
         boxSizing="border-box"
         height="40vh"
-        bg="#fff"
+        bg="white"
         p="2"
         mb="3"
         borderRadius="5px"
+        color="gray.500"
         dangerouslySetInnerHTML={{ __html: marked(note.description) }}
       ></Text>
       <Flex>
         <ActionButton
           onClick={() => dispatch(setNoteView(NOTE_EDIT_VIEW))}
           type="submit"
-          color="#fc5c9c"
+          color="pink.500"
           mr="2"
           text="Edit"
         />
         <ActionButton
           onClick={() => dispatch(setNoteView(NOTE_LIST_VIEW))}
-          color="grey"
+          color="gray.500"
           text="View Notes"
         />
       </Flex>
